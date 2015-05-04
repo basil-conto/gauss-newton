9d5820292c2bdacd69659aa1eac56d9716ac0457
Basil L. Contovounesios <contovob@tcd.ie>
2015-05-04 13:41:52 +0100

    New graphs

--- graph.py
+++ graph.py
@@ -18,23 +18,62 @@ __email__   = "contovob@tcd.ie"
 __version__ = "2015.05.03"
 __license__ = "BSD3"
 
-def save(sys, x, title = None, fname = "plot.pdf", odir = ""):
-    (ans0, _), (ans1, _) = [gn.solve(sys, start) for start in sys.starts]
-    plt.figure()
-    plt.plot(x, sys.model(x, ans0),
-             x, sys.model(x, ans1),
-             x, sys.model(x))
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.legend(["Start1", "Start2", "Certified"], loc = "best")
-    plt.title(title)
-    plt.savefig(odir + fname, bbox_inches = "tight")
-    plt.close()
-
 odir = "img/"
+size = 10, 8
 
-save(m, m.xvals, "Misra1a Solution - Observed Range", "misra1a-obs.pdf", odir)
-save(m, np.arange(-2048, 2048), "Misra1a Solution", "misra1a.pdf", odir)
+# Misra1a ----------------------------------------------------------------------
+(ans0, _), (ans1, _) = [gn.solve(m, start) for start in m.starts]
 
-save(t, t.xvals, "Thurber Solution - Observed Range", "thurber-obs.pdf", odir)
-save(t, np.arange(-25, 25), "Thurber Solution", "thurber.pdf", odir)
+# Observed range
+x = m.xvals
+plt.figure(figsize = size)
+plt.plot(x, m.yvals, "c.",
+         x, m.model(x, ans0),
+         x, m.model(x, ans1),
+         x, m.model())
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend(["Observations", "Start 1", "Start 2", "Certified"], loc = "best")
+plt.savefig(odir + "misra1a-obs.pdf", bbox_inches = "tight")
+plt.close()
+
+# Extended range
+x = np.arange(-2048, 2048)
+plt.figure(figsize = size)
+plt.plot(x, m.model(x, ans0),
+         x, m.model(x, ans1),
+         x, m.model(x))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend(["Start 1", "Start 2", "Certified"], loc = "best")
+plt.savefig(odir + "misra1a.pdf", bbox_inches = "tight")
+plt.close()
+
+# Thurber ----------------------------------------------------------------------
+(ans0, _), (ans1, _) = [gn.solve(t, start) for start in t.starts]
+
+# Observed range
+x = t.xvals
+plt.figure(figsize = size)
+plt.plot(x, t.yvals, "c.",
+         x, t.model(x, ans0),
+         x, t.model(x, ans1),
+         x, t.model())
+plt.xlabel("x")
+plt.ylabel("y")
+plt.ylim(ymin = 0)
+plt.legend(["Observations", "Start 1", "Start 2", "Certified"], loc = "best")
+plt.savefig(odir + "thurber-obs.pdf", bbox_inches = "tight")
+plt.close()
+
+# Extended range
+x = np.arange(-25, 25)
+plt.figure(figsize = size)
+plt.plot(x, t.model(x, ans0),
+         x, t.model(x, ans1),
+         x, t.model(x))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend(["Start 1", "Start 2", "Certified"], loc = "best")
+plt.savefig(odir + "thurber.pdf", bbox_inches = "tight")
+plt.close()
